260f1335bafb3cc14e3ed670affe58c7795f7d3a
Sarfaroj Ansari <sarfarosha66@gmail.com>
2023-09-24 14:52:27 +0530

    initial commit with stater files and basic code

---
+++ index.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const myLibrary = [
+        {
+            author: "Norman D. Lewis",
+            title: "Wordpower Made Easy",
+            totalPages: "600", 
+            read: false
+        }
+    ];
+
+    function Book() {
+        this.author = author;
+        this.title = title;
+        this.totalPages = totalPages;
+    }
+
+    function addBookToLibrary() {
+        // 
+        const showButton = document.getElementById('dialog');
+        const dialog = document.getElementById('newBookDialog');
+    
+    
+        showButton.addEventListener('click', () => {
+            dialog.showModal()
+        })
+    }
+
+    // display books in the table
+    const tbody = document.getElementById('booksInfoBody');
+
+
+    for (let book of myLibrary) {
+        objLength = Object.keys(book).length;
+        
+        let tr = document.createElement('tr');
+
+        // for (let i = 0; i < objLength; i++) {
+        //     let td = document.createElement('td');
+        //     td.innerHTML = `${book[i]}`
+        //     tr.appendChild(td);
+        // }
+
+        for (let key in book) {
+            let td = document.createElement('td');
+            td.innerHTML = `${book[key]}`
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr)
+    }
+
+})
