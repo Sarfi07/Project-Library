df42d94395dcd47c8fe5bc5a8fa5a560281ccc61
Sarfaroj Ansari <sarfarosha66@gmail.com>
2023-09-26 23:24:53 +0530

    moved to new branch to write better JS

--- index.js
+++ index.js
@@ -1,202 +1,80 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+    const myLibrary = [];
 
-    const myLibrary = [
-        {
-            title: "Wordpower Made Easy",
-            author: "Norman D. Lewis",
-            totalPages: "600", 
-            read: false
-        }
-    ];
+    function Book(title, author, totalPages, read) {
+        // the constructor...
 
-    function Book(author, title, totalPages, read) {
-        this.author = author;
         this.title = title;
+        this.author = author;
         this.totalPages = totalPages;
-        this.read = read
+        this.read = read;
+
+        this.info = () => {
+            if (read == true) {
+                let readBool = "read already!";
+            } else {
+                readBool = "not read yet"
+            }
+            return (`${title} by ${author}, ${totalPages} pages, ${readBool}`);
+        }
+
     }
 
     function addBookToLibrary() {
-        // 
-        const showButton = document.getElementById('dialog');
-        const dialog = document.getElementById('newBookDialog');
         const form = document.getElementById('newBookForm');
-    
-    
-        showButton.addEventListener('click', () => {
-            dialog.showModal()
-        })
+        let title = document.getElementById('title');
+        let author = document.getElementById('author');
+        let totalPages = document.getElementById('totalPages');
+        let read = document.getElementById('read');
 
-        // form submission
+
         form.addEventListener('submit', (event) => {
-            // event.preventDefault();
-            let author = document.getElementById('author').value;
-            let title = document.getElementById('title').value;
-            let pages = document.getElementById('totalPages').value;
-            let completed = document.getElementById('read').value;
-            let tbody = document.getElementById('booksInfoBody');
-            
-            if (completed == "true") {
-                completed = true;
-            } else completed = false;
 
-
-            const newBook = new Book(title, author, pages, completed);
-
-            myLibrary.push(newBook)
-
-            
-            // append on the page
-            let tr = document.createElement('tr');
-
-            for (let key in newBook) {
-                let td = document.createElement('td');
-
-                if (newBook[key] == true || newBook[key] == false) {
-                    var btn = document.createElement('button');
-                    btn.style.fontSize = "1rem";
-                    btn.style.margin = ".25rem";
-                    btn.style.padding = ".35rem";
-                    btn.style.fontWeight = "600";
-                    btn.innerText = `${newBook[key]}`;
-                    btn.setAttribute('data-index', index);
-                    btn.setAttribute('class', 'readBtn');
-                    td.appendChild(btn);
-
-                    btn.addEventListener('click', () => {
-                        if (newBook[key] == true) {
-                            // change the text
-                                btn.innerText = "false"
-                            // change the object
-                                myLibrary[index].read = false;
-                        } else {
-                            btn.innerText = "True";
-                            myLibrary[index].read = true
-                        }
-                    })
-                } else {
-                    td.innerHTML = `${newBook[key]}`
-                }
-                tr.appendChild(td);
+            if (read.value == 'true') {
+                read = true;
+            } else {
+                read = false
             }
 
-            // addding delete button
-            let delTd = document.createElement('td');
-            let delBtn = document.createElement('button');
-            var index = myLibrary.indexOf(newBook)
-            delBtn.setAttribute('data-index', index)
-            delBtn.innerText = "Delete";
-            delBtn.setAttribute('class', 'deleteBtn');
-            delBtn.style.fontSize = "1rem";
-            delBtn.style.margin = ".25rem";
-            delBtn.style.padding = ".35rem";
-            delTd.appendChild(delBtn)
-
-            tr.appendChild(delTd);
-            tr.setAttribute('id', index)
-
-            tbody.appendChild(tr);
+            const newBook = new Book(title.value, author.value, totalPages.value, read);
             
-            delBtn.addEventListener('click', () => {
-                myLibrary.splice(index, 1);
-               tr.style.display = 'none' ;
-            })
+
+            myLibrary.push(newBook);
         })
+
     }
 
-    addBookToLibrary();
 
-    // display books in the table
+    function showDialog() {
+        const showBtn = document.getElementById('dialogBtn');
+        const dialog = document.getElementById('newBookDialog');
+
+        showBtn.addEventListener('click', () => {
+            dialog.showModal();
+        })
+
+    }
+
 
     function displayBooks() {
+
         const tbody = document.getElementById('booksInfoBody');
-    
-    
-        for (let book of myLibrary) {
+
+        for (book of myLibrary) {
             
-            if (myLibrary.length == 0) {
-                console.log(myLibrary.length)
-                return
-            }
-            let tr = document.createElement('tr');
- 
-            for (let key in book) {
-                let td = document.createElement('td');
+            // create multiple td element and a tr element
+            const tr = document.createElement('tr');
+            const titleTd = document.createElement('td');
+            const authorTd = document.createElement('td');
+            const totalPagesTd = document.createElement('td');
+            const readTd = document.createElement('td');
+            const deleteBtnTd = document.createElement('td');
 
-                // adding a toggle button on read key
-                if (book[key] == true || book[key] == false) {
-                    var btn = document.createElement('button');
-                    btn.style.fontSize = "1rem";
-                    btn.style.margin = ".25rem";
-                    btn.style.padding = ".35rem";
-                    btn.style.fontWeight = "600";
-                    btn.innerText = `${book[key]}`;
-                    btn.setAttribute('data-index', index);
-                    btn.setAttribute('class', 'readBtn');
-                    td.appendChild(btn);
+            // TODO
 
-                    btn.addEventListener('click', () => {
-                        if (book[key] == true) {
-                            // change the text
-                                btn.innerText = "false"
-                            // change the object
-                                myLibrary[index].read = false;
-                        } else {
-                            btn.innerText = "True";
-                            myLibrary[index].read = true
-                        }
-                    })
-                } else {
-                    td.innerHTML = `${book[key]}`;
-                }
-
-                // set data attribute which correspond to index on the book
-                
-                tr.appendChild(td);
-
-            }
-
-            // addding delete button
-            let delTd = document.createElement('td');
-            let delBtn = document.createElement('button');
-            var index = myLibrary.indexOf(book);
-            delBtn.setAttribute('data-index', index);
-            delBtn.setAttribute('class', 'deleteBtn');
-            delBtn.innerText = "Delete";
-            delBtn.style.fontSize = "1rem";
-            delBtn.style.margin = ".25rem";
-            delBtn.style.padding = ".35rem";
-
-            delTd.appendChild(delBtn)
-
-            tr.appendChild(delTd);
-            tr.setAttribute('id', index)
-            tbody.appendChild(tr);
-
-            // delete book
-            deleteBook();
         }
     }
-    
 
-    function deleteBook() {
-        let btns = document.querySelectorAll('.deleteBtn');
-
-        btns.forEach((btn) => {
-            btn.addEventListener('click', () => {
-                var index = btn.dataset.index;
-                
-                // delete book from the myLibrary
-                myLibrary.splice(index, 1)
-
-                // showing remaining books
-                let currentTr = document.getElementById(index);
-                currentTr.style.display = 'none';
-                
-            })
-        })
-    }
-
-
-    displayBooks();
+    showDialog();
+    addBookToLibrary();
 })
